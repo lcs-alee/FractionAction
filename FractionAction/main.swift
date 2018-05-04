9feca76915b9c01ef0e4ade073ee5b43b3b0c6ef
Lee Andrew <alee@lcs-mba3087.lcs.on.ca>
2018-05-04 09:46:19 -0400

    1

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -16,9 +16,6 @@ var numerator = 0
 //Global denominator
 var denominator = 0
 
-//Global remainder
-var remainder = 0
-
 //Loop until valid numerator input is found
 while 2 == 2 {
     
@@ -87,52 +84,56 @@ while 2 == 2 {
 // PROCESS
 // Implement the primary logic of the problem here
 //Find whole number
-let wholeNumber = numerator/denominator
+let quotient = numerator / denominator
+let remainder = numerator % denominator
 
-// Simplify
-// If numerator == denominator, the answer will be a whole number
-// If numerator == 0, answer = 0
-// Both these have no fractions, so they can be skipped
-if numerator == denominator || numerator == 0 {
-    
-} else {
-    // Find fraction
-    
-    // Remainder after the whole number is
-    remainder = numerator - wholeNumber * denominator
-    // Simplify
-    // Find the common factors of the remainder and the denominator
-    for i in stride(from: remainder, to: 2, by: -1) {
-        
-        // Divide the remainder and the denominator by the common factor
-        if remainder%i == 0 && denominator%i == 0{
-            denominator /= i
-            remainder /= i
-            continue
-        }
-    }
-}
+print("The result is:")
+print(quotient)
 
+//// Simplify
+//// If numerator == 0,
+//// there are no fractions, so this can be skipped
+//if numerator == 0 {
+//
+//} else {
+//    // Find fraction
+//
+//    // Remainder after the whole number is
+//    remainder = numerator - wholeNumber * denominator
+//    // Simplify
+//    // Find the common factors of the remainder and the denominator
+//    for i in stride(from: remainder, through: 2, by: -1) {
+//
+//        // Divide the remainder and the denominator by the common factor
+//        if remainder%i == 0 && denominator%i == 0{
+//            denominator /= i
+//            remainder /= i
+//            continue
+//        }
+//    }
+//}
+//
+//
+//
+//// OUTPUT
+//// Report results to the user here
+//// Print out the answer (No fraction)
+//if remainder == 0 {
+//    print("The result is \(wholeNumber)")
+//}
+//    // No whole number
+//else if wholeNumber == 0 && remainder > 0
+//{
+//    print("The result is \(remainder)/\(denominator)")
+//}
+//    // Mixed fraction
+//else if wholeNumber > 0 && remainder > 0
+//{
+//    print("The result is \(wholeNumber) \(remainder)/\(denominator)")
+//}
+//    // Answer is 0
+//else
+//{
+//    print("The result is 0")
+//}
 
-
-// OUTPUT
-// Report results to the user here
-// Print out the answer (No fraction)
-if remainder == 0 {
-    print("The result is \(wholeNumber)")
-}
-    // No whole number
-else if wholeNumber == 0 && remainder > 0
-{
-    print("The result is \(remainder)/\(denominator)")
-}
-    // Mixed fraction
-else if wholeNumber > 0 && remainder > 0
-{
-    print("The result is \(wholeNumber) \(remainder)/\(denominator)")
-}
-    // Answer is 0
-else
-{
-    print("The result is 0")
-}
